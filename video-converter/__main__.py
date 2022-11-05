88d2c435d79f6e5e523c10d62d60601fcad34b6f
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2022-11-05 14:28:18 +0200

    refactor

--- video-converter/__main__.py
+++ video-converter/__main__.py
@@ -1,7 +1,12 @@
-import ffmpegsetup
-import VideoConverter
+# import ffmpegSetup
+# import VideoConverter
+#
+# if __name__ == '__main__':
+#     ffmpegSetup.setup()
+#     VideoConverter.main
 
-if __name__ == '__main__':
-    ffmpegsetup.setup()
-    VideoConverter.main()
 
+from Programs import ffmpeg
+
+#run ffmpeg module
+ffmpeg.unzip()
