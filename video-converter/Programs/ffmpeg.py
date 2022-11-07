3a851e340cad89919baea084abaee7ccc12bcc0b
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2022-11-07 16:35:51 +0200

    rename ffmpeg from zip file itself

--- video-converter/Programs/ffmpeg.py
+++ video-converter/Programs/ffmpeg.py
@@ -3,6 +3,7 @@
 import os
 import zipfile
 
+
 def unzip_file(zip_file, unzip_dir):
     if not os.path.exists(os.path.abspath(os.getcwd()) + '\\ffmpeg'):
         if not os.path.exists(unzip_dir):
@@ -10,11 +11,5 @@ def unzip_file(zip_file, unzip_dir):
         with zipfile.ZipFile(zip_file, 'r') as zip_ref:
             zip_ref.extractall(unzip_dir)
 
-# rename folder in python
-def rename_folder(file):
-    if os.path.exists(file):
-        os.rename(file, 'ffmpeg')
 
-def unzip():
-    unzip_file('ffmpeg.zip', os.path.abspath(os.getcwd()))
-    rename_folder('ffmpeg-2022-10-30-git-ed5a438f05-essentials_build')
+unzip_file('ffmpeg.zip', os.path.abspath(os.getcwd()))
