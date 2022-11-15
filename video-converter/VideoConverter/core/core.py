6b473c5455b6322b4fff9af077f696f469bbdcd8
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2022-11-15 07:07:15 +0200

    modfiy core

--- video-converter/VideoConverter/core/core.py
+++ video-converter/VideoConverter/core/core.py
@@ -5,6 +5,7 @@
 
 import os
 import pathlib
+import shutil
 
 
 # convert video to .wav file by using ffmpeg
@@ -70,15 +71,36 @@ class videoconverter:
         else:
             print("video not found")
 
+    def copytree_audio(src, symlinks=False, ignore=None):
+        dst = os.path.join(src, "output")
+        if os.path.exists(dst) == False:
+            os.mkdir(dst)
+
+        for item in os.listdir(src):
+            if os.path.exists(item) == False and item.endswith(
+                    ".wav" or ".mp3" or ".m4a" or ".flac" or ".aac" or ".ogg" or ".wma" or ".aiff" or ".alac" or ".amr" or ".ape" or ".au" or ".dct" or ".dss" or ".dvf" or ".flac" or ".gsm" or ".iklax" or ".ivs" or ".m4a" or ".m4b" or ".m4p" or ".mmf" or ".mp3" or ".mpc" or ".msv" or ".nmf" or ".nsf" or ".ogg" or ".oga" or ".mogg" or ".opus" or ".ra" or ".rm" or ".raw" or ".rf64" or ".sln" or ".tta" or ".vox" or ".wav" or ".wma" or ".wv" or ".webm" or ".8svx" or ".cda" or ".mid" or ".midi" or ".rmi" or ".kar" or ".mka" or ".aif" or ".aifc" or ".aiff" or ".m3u" or ".wpl" or ".asx" or ".pls" or ".xspf" or ".m3u" or ".wpl" or ".asx" or ".pls" or ".xspf" or ".mp4" or ".m4v" or ".avi" or ".mov" or ".flv" or ".wmv" or ".mpg" or ".mpeg" or ".3gp" or ".webm" or ".vob" or ".m4v" or ".rmvb" or ".rm" or ".asf" or ".swf" or ".m2ts" or ".mts" or ".ts" or ".ogv" or ".ogg" or ".mxf" or ".dv" or ".dvr-ms" or ".amv" or ".qt" or ".f4v" or ".flv" or ".f4p" or ".f4a" or ".f4b"):
+                s = os.path.join(src, item)
+                d = os.path.join(dst, item)
+                if os.path.isdir(s):
+                    shutil.copytree(s, d, symlinks, ignore)
+                else:
+                    shutil.copy2(s, d)
+
     # Convert all videos in given directory to .wav files
     def convert(given_path):
 
-        myfile = pathlib.Path(given_path)
+        # make output directory
 
-        if myfile.isdir(given_path):
+        # copy all audio files to output directory
+        videoconverter.copytree_audio(given_path)
+
+        if pathlib.Path(given_path).is_file() == False:
             video_paths = videoconverter.get_videos_paths(given_path)
             for video_path in video_paths:
                 videoconverter.video_to_wav(video_path)
 
         else:
             videoconverter.video_to_wav(given_path)
+
+
+videoconverter.convert("D:\music")
